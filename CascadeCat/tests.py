1eb91678f2e928e0294c5733f3a0292b4a81b390
amir shahsafi <xvstanxv@gmail.com>
2021-01-24 22:26:25 +0330

    test implementation

--- CascadeCat/tests.py
+++ CascadeCat/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Product, Category, SubCategory
 
-# Create your tests here.
+
+class TestForCreateProduct(TestCase):
+    def setUp(self):
+        product = Product.objects.create(name='test product',
+                                barcode='21314151',
+                                category__id=1
+                                )
+
+    def test_animals_can_speak(self):
+        """Animals that can speak are correctly identified"""
+        lion = Animal.objects.get(name="lion")
+        cat = Animal.objects.get(name="cat")
+        self.assertEqual(lion.speak(), 'The lion says "roar"')
+        self.assertEqual(cat.speak(), 'The cat says "meow"')
