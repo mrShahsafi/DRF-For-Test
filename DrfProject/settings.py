8cf9c388fcf4ee21433ccfec0090c6bae58607eb
amir shahsafi <xvstanxv@gmail.com>
2021-02-14 16:26:38 +0330

    allow cors headers for Front-End

--- DrfProject/settings.py
+++ DrfProject/settings.py
@@ -48,19 +48,26 @@ INSTALLED_APPS = [
     'rest_framework_swagger',
     'u',
     'CascadeCat',
-#    'rest_auth',
+#    'rest_auth',,
+    'corsheaders'
     ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+#allow cors origing
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = [
+'http://localhost:3000',
+'http://127.0.0.1:3000'
+]
 ROOT_URLCONF = 'DrfProject.urls'
 
 TEMPLATES = [
